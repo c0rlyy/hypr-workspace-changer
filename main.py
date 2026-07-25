1779715ee9ef97b84bf53a23a279c3dcdb15a56a
c0rlyyy <oneNightHell199@proton.me>
2026-07-25 23:38:31 +0200

    added error notifications

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
 import json
 import subprocess
 import sys
-from pathlib import Path
 from typing import Any
-
-# legacy but left
-CONFIG_PATH = Path.home() / ".config" / "hypr" / "monitors.conf"
-
-
-def read_config_file(path: Path):
-    """legacy"""
-    config_lines = []
-    with open(path, "r") as f:
-        for line in f:
-            line = line.strip()
-            if ("workspace =" in line or "workspace=" in line) and "#" not in line:
-                config_lines.append(line)
-    return config_lines
-
-
-def parse_config_lines(config_lines: list[str]) -> dict[str, str]:
-    """legacy"""
-    parsed_lines = {}
-    for line in config_lines:
-        parts = [p.strip() for p in line.split(",")]
-        monitor = ""
-        workspace = ""
-        for part in parts:
-            if part.startswith("monitor:"):
-                monitor = part.split(":", 1)[1].strip()
-            elif part.startswith("workspace"):
-                workspace = part.split("=", 1)[1].strip()
-        if monitor:
-            parsed_lines.setdefault(monitor, []).append(workspace)
-    return parsed_lines
 
 
 def get_focused_monitor() -> Any:
@@ -44,7 +12,6 @@ def get_focused_monitor() -> Any:
     for m in monitors:
         if m["focused"]:
             return m["name"]
-    return None
 
 
 def get_config_from_cmd():
@@ -60,6 +27,9 @@ def get_config_from_cmd():
     return monitors_workspaces
 
 
+def notify(msg: str) -> None:
+    subprocess.run(["notify-send", "workspace-changanger", msg], check=False)
+
 def switch_workspace(
     key: str, current_monitor: str, parsed_config: dict[str, list[str]]
 ):
@@ -71,7 +41,12 @@ def switch_workspace(
 
 
 if __name__ == "__main__":
-    parsed_config = get_config_from_cmd()
-    key = sys.argv[1]
-    current_monitor = get_focused_monitor()
-    switch_workspace(key, current_monitor, parsed_config)
+    try:
+        parsed_config = get_config_from_cmd()
+        key = sys.argv[1]
+        current_monitor = get_focused_monitor()
+        switch_workspace(key, current_monitor, parsed_config)
+    except Exception as e:
+        msg = "error: " + str(e)
+        notify(msg)
+        sys.exit(1)
